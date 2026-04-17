dd785cf2e2c219315a15827e1f73af2c57daf1e9
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2026-04-16 19:08:20 -0700

    Revert "[MR] [Update] Fix blank map in ios build (#20)" (#22)
    This reverts commit c80aae0b4c212cf0efe1094664a25a1b0ecbf75c.

--- utils/formatters.ts
+++
@@ -1,9 +0,0 @@
-export const formatDateShortVN = (dateString?: string | null) => {
-  if (!dateString) return "Chưa có";
-
-  const date = new Date(dateString);
-  if (Number.isNaN(date.getTime())) return "Chưa có";
-
-  return date.toLocaleDateString("vi-VN");
-};
-
